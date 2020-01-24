2d40cd23438b6fa29121ff68a554693c55d3356a
privatelancelot <this@nomail.com>
2020-01-24 11:23:40 +0000

    removed final from git

--- YelpCamp/final/models/user.js
+++
@@ -1,7 +0,0 @@
-var passportLocalMongoose = require("passport-local-mongoose"),
-	mongoose = require("mongoose");
-
-module.exports = mongoose.model("User", new mongoose.Schema({
-	username: String,
-	password: String
-}).plugin(passportLocalMongoose));
