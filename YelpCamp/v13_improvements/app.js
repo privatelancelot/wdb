fbdf97ccf8a4d346503a8afc03ddb5fb5e30c925
privatelancelot <59507271+privatelancelot@users.noreply.github.com>
2020-10-31 10:31:42 +0200

    Update app.js to re-seed db each run

--- YelpCamp/v13_improvements/app.js
+++ YelpCamp/v13_improvements/app.js
@@ -34,7 +34,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-//seedDB();
+seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
